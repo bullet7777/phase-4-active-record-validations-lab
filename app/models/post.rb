b15aedc5b767682db160fb496ea1a68496d42938
chris <chris.mahfouz@hotmail.com>
2021-06-17 12:35:12 +0300

    commit tree 91dd861471c48850c1a745c5673385bdad635180

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title,presence:true
+    validates :content,length: {minimum:250}
+    validates :summary,length: {maximum:250}
+    validates :category,inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_have
+    CLICKBAIT= [
+    "Won't Believe",
+    "Secret",
+    "Top[number]",
+    "Guess"
+]
+
+def title_have
+    if title.present? &&  CLICKBAIT.none? {|p| title.match(p)}
+        errors.add(:title, "title not clickbait-y ")
+    end
+end
+ 
+
 end
